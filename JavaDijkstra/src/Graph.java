a4481c5a1f74c9e745318355c08276ba8fa48173
crockie <57024535+crockie@users.noreply.github.com>
2022-10-11 13:18:46 +0800

    Dijkstra in Java

---
+++ JavaDijkstra/src/Graph.java
@@ -0,0 +1,171 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Graph {
+
+    int V;
+    int E;
+    LinkedList<Edge>[] adjList;
+    int adjMatrix[][];
+
+    public Graph(int V) {
+        this.V = V;
+        adjList = new LinkedList[V];
+        for (int i = 0; i < V; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+        adjMatrix = new int[V][V];
+    }
+
+    /**
+     * Constructor to generate random graph
+     * 
+     * @param maxVertices maximum number of vertices in the graph
+     * @param maxWeight   maximum weight of an edge
+     */
+    public Graph(int maxVertices, int maxWeight, int sparseFactor) {
+
+        if (sparseFactor == 0) {
+            Random rand = new Random();
+            this.V = maxVertices;
+            adjList = new LinkedList[V];
+            for (int i = 0; i < V; i++) {
+                adjList[i] = new LinkedList<>();
+            }
+            adjMatrix = new int[V][V];
+
+            /*
+             * For a randomly chosen number of vertices V,
+             * the maximum number of possible edges is
+             * V*(V – 1)(with no multiple edges and self-loops).
+             */
+
+            int maxEdges = V * (V - 1);
+            int randEdges = maxEdges;
+            for (int i = 0; i < randEdges; i++) {
+                int src = rand.nextInt(V);
+                int dest = rand.nextInt(V);
+                if (src != dest && !existEdge(src, dest)) {
+                    int weight = rand.nextInt(maxWeight) + 1;
+                    addEdge(src, dest, weight);
+                } else
+                    i--;
+            }
+        }
+        if (sparseFactor == 1) {
+            Random rand = new Random();
+            this.V = maxVertices;
+            adjList = new LinkedList[V];
+            for (int i = 0; i < V; i++) {
+                adjList[i] = new LinkedList<>();
+            }
+            adjMatrix = new int[V][V];
+
+            /*
+             * For a randomly chosen number of vertices V,
+             * the maximum number of possible edges is
+             * V*(V – 1)(with no multiple edges and self-loops).
+             */
+
+            int maxEdges = V * (V - 1);
+            int minEdges = V - 1;
+            int randEdges = rand.nextInt(maxEdges-minEdges) + minEdges;
+            for (int i = 0; i < randEdges; i++) {
+                int src = rand.nextInt(V);
+                int dest = rand.nextInt(V);
+                if (src != dest && !existEdge(src, dest)) {
+                    int weight = rand.nextInt(maxWeight) + 1;
+                    addEdge(src, dest, weight);
+                } else
+                    i--;
+            }
+        }
+        else{   
+            // generate sparse graph
+            Random rand = new Random();
+            this.V = rand.nextInt(maxVertices) + 2;
+            adjList = new LinkedList[V];
+            for (int i = 0; i < V; i++) {
+                adjList[i] = new LinkedList<>();
+            }
+            adjMatrix = new int[V][V];
+
+            /*
+             * For a randomly chosen number of vertices V,
+             * the maximum number of possible edges is
+             * V*(V – 1)(with no multiple edges and self-loops).
+             */
+
+            int edgeCount = V-1; //minimum E for a connected graph
+            for (int i = 0; i < edgeCount; i++) {
+                int src = rand.nextInt(V);
+                int dest = rand.nextInt(V);
+                if (src != dest && !existEdge(src, dest)) {
+                    int weight = rand.nextInt(maxWeight) + 1;
+                    addEdge(src, dest, weight);
+                } else
+                    i--;
+            }
+
+        }
+    }
+
+    public void addEdge(int src, int dest, int weight) {
+        Edge edge = new Edge(src, dest, weight);
+        adjList[src].addFirst(edge);
+        adjMatrix[src][dest] = weight;
+        E++;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < V; i++) {
+            LinkedList<Edge> list = adjList[i];
+            for (int j = 0; j < list.size(); j++) {
+                System.out.println("vertex " + i + " is connected to " + list.get(j).dest + " with weight "
+                        + list.get(j).weight);
+            }
+        }
+    }
+
+    public void printGraphMatrix(Graph g)
+    {
+        int i,j;
+        System.out.println("Print graph matrix \n");
+
+        for(i=0;i<V;i++){
+            System.out.print("Vertex " + i + ":\t");
+            for(j=0;j<V;j++)
+                System.out.print(g.adjMatrix[i][j] + "\t");
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+
+    }
+
+    public void printGraphList(Graph g){
+        int i;
+        LinkedList<Edge> temp;
+        System.out.println("Print graph list");
+    
+        for(i=0;i<V;i++)
+        {
+            System.out.print(i + "\t");
+            temp = g.adjList[i];
+
+            for (int j = 0; j < temp.size(); j++) {
+                System.out.print(temp.get(j).dest +" w " +temp.get(j).weight + "-> ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean existEdge(int src, int dest) {
+        // check if there is alreeady an edge between src and dest
+        if (adjMatrix[src][dest] != 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ JavaDijkstra/src/Test.java
@@ -0,0 +1,222 @@
+import java.io.IOException;
+
+public class Test {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        // *unncomment the following line to run the respective test program
+
+        // testPrintGraph();
+        //testDijkstraArray();
+        //testDijkstraPQ();
+        //testGenerateRandomGraph(5,10);
+        //empiricalTest(150, 1000, 10, 1000);
+        empiricalTestSparse(100,1000,1000,1000);
+    }
+
+    /**
+     * Test the printGraph method
+     */
+    private static void testPrintGraph() {
+        Graph g = new Graph(5);
+        g.addEdge(0, 1, 4);
+        g.addEdge(0, 2, 2);
+        g.addEdge(0, 3, 6);
+        g.addEdge(0, 4, 8);
+        g.addEdge(1, 3, 4);
+        g.addEdge(1, 4, 3);
+        g.addEdge(2, 3, 1);
+        g.addEdge(3, 1, 1);
+        g.addEdge(3, 4, 3);
+        g.printGraph();
+        System.out.println();
+    }
+
+    /**
+     * Test the Dijkstra algorithm using an adjMatrix and Array
+     */
+    private static void testDijkstraArray() {
+        System.out.println("teesDijkstraArray: ");
+        Graph g = new Graph(5);
+        g.addEdge(0, 1, 4);
+        g.addEdge(0, 2, 2);
+        g.addEdge(0, 3, 6);
+        g.addEdge(0, 4, 8);
+        g.addEdge(1, 3, 4);
+        g.addEdge(1, 4, 3);
+        g.addEdge(2, 3, 1);
+        g.addEdge(3, 1, 1);
+        g.addEdge(3, 4, 3);
+        g.printGraphMatrix(g);
+        g.printGraphList(g);
+        DijkstraAlgo dijkstraTest = new DijkstraAlgo(5);
+        dijkstraTest.dijkstraArrayStart(5, 0, g.adjMatrix);
+    }
+
+    /**
+     * Test the Dijkstra algorithm using an adjList and a PriorityQueue
+     */
+    private static void testDijkstraPQ() {
+        System.out.println("testDijkstraPQ: ");
+        Graph g = new Graph(5);
+        g.addEdge(0, 1, 4);
+        g.addEdge(0, 2, 2);
+        g.addEdge(0, 3, 6);
+        g.addEdge(0, 4, 8);
+        g.addEdge(1, 3, 4);
+        g.addEdge(1, 4, 3);
+        g.addEdge(2, 3, 1);
+        g.addEdge(3, 1, 1);
+        g.addEdge(3, 4, 3);
+        /*g.addEdge(0, 1, 4);
+        g.addEdge(0, 2, 2);
+        g.addEdge(0, 3, 6);
+        g.addEdge(0, 4, 8);
+        g.addEdge(1, 3, 4);
+        g.addEdge(1, 4, 3);
+        g.addEdge(2, 3, 1);
+        g.addEdge(3, 1, 1);
+        g.addEdge(3, 4, 3);
+        */
+        g.printGraphList(g);
+        DijkstraAlgo dijkstraTest = new DijkstraAlgo(5);
+        dijkstraTest.dijkstraPQStart(5, 0, g.adjList);
+    }
+
+    /**
+     * Test the generateRandomGraph method
+     * 
+     * @param maxVertices maximum number of vertices
+     * @param maxEdges    maximum number of edges
+     */
+    private static void testGenerateRandomGraph(int maxVertices, int maxWeight) {
+        Graph g = new Graph(maxVertices, maxWeight, 0);
+        System.out.println("Random Graph generated, V = " + g.V + ", E = " + g.E);
+        g.printGraphList(g);
+        g.printGraphMatrix(g);
+    }
+
+    /**
+     * Test the Dijkstra algorithm on a random graph in two ways: adjMatrix+Array
+     * and adjList+PQ. During the test, runtime (in nanoseconds) is recorded and
+     * stored.
+     * This test shall generate 4 .csv files, containing the results of the
+     * empirical test: verticeArr.csv, edgeArr.csv,
+     * timeArr(test1).csv, timeArr(test2).csv
+     * 
+     * @param maxVertices  maximum number of vertices for random graph generation
+     * @param maxWeight    maximum weight for random graph generation
+     * @param graphCount   number of graphs to be generated
+     * @param averageTimes number of times to run the algorithm for each graph
+     * @throws IOException
+     */
+    private static void empiricalTest(int maxVertices, int maxWeight, int graphCount, int averageTimes)
+            throws IOException {
+        
+        // for connected graph w varying vertices
+        long[] verticeArr = new long[maxVertices], edgeArr = new long[maxVertices], timeArr1 = new long[maxVertices],
+                timeArr2 = new long[maxVertices];
+                        
+        for(int i = 1; i <= maxVertices; i++){
+            System.out.println("Generating max vertex: " + i);
+            Graph g = new Graph(i, maxWeight, 0);
+            verticeArr[i-1] = g.V;
+            edgeArr[i-1] = g.E;
+            long timeTotal1 = 0;
+            long timeTotal2 = 0;
+            for (int j = 0; j < averageTimes; j++) {
+                DijkstraAlgo dijkstraTest = new DijkstraAlgo(g.V);
+                long startTime = System.nanoTime();
+                dijkstraTest.dijkstraArrayStart(g.V, 0, g.adjMatrix);
+                long endTime = System.nanoTime();
+                timeTotal1 += endTime - startTime;
+
+                startTime = System.nanoTime();
+                dijkstraTest.dijkstraPQStart(g.V, 0, g.adjList);
+                endTime = System.nanoTime();
+                timeTotal2 += endTime - startTime;
+            }
+            timeArr1[i-1] = timeTotal1 / averageTimes;
+            timeArr2[i-1] = timeTotal2 / averageTimes;
+        }
+
+        MakeCSV.CSVprinter(verticeArr, "verticeArr.csv");
+        MakeCSV.CSVprinter(edgeArr, "edgeArr.csv");
+        MakeCSV.CSVprinter(timeArr1, "timeArr(test1).csv");
+        MakeCSV.CSVprinter(timeArr2, "timeArr(test2).csv");
+
+        /*long[] verticeArr = new long[graphCount*maxVertices], edgeArr = new long[graphCount*maxVertices], timeArr1 = new long[graphCount*maxVertices],
+                timeArr2 = new long[graphCount*maxVertices];
+        for (int i = 0; i < graphCount; i++) {
+            System.out.println("Generating graph " + (i + 1) + "...");
+            for(int k = 1; k <= maxVertices; k++){
+                Graph g = new Graph(k, maxWeight, 0);
+                verticeArr[i*k+k-1] = g.V;
+                edgeArr[i*k+k-1] = g.E;
+                long timeTotal1 = 0;
+                long timeTotal2 = 0;
+                for (int j = 0; j < averageTimes; j++) {
+                    DijkstraAlgo dijkstraTest = new DijkstraAlgo(g.V);
+                    long startTime = System.nanoTime();
+                    dijkstraTest.dijkstraArrayStart(g.V, 0, g.adjMatrix);
+                    long endTime = System.nanoTime();
+                    timeTotal1 += endTime - startTime;
+    
+                    startTime = System.nanoTime();
+                    dijkstraTest.dijkstraPQStart(g.V, 0, g.adjList);
+                    endTime = System.nanoTime();
+                    timeTotal2 += endTime - startTime;
+                }
+                timeArr1[i*k+k-1] = timeTotal1 / averageTimes;
+                timeArr2[i*k+k-1] = timeTotal2 / averageTimes;
+            }
+            
+        }
+
+        for(int i = 0; i < graphCount; i++){
+            for(int j = 1; j <=  maxVertices; j++)
+                System.out.print(verticeArr[i*j+j-1] + " ");
+            System.out.println();
+        }*/
+        
+           
+        
+    }
+
+    private static void empiricalTestSparse(int maxVertices, int maxWeight, int graphCount, int averageTimes)
+            throws IOException {
+        long[] verticeArr = new long[graphCount], edgeArr = new long[graphCount], timeArr1 = new long[graphCount],
+                timeArr2 = new long[graphCount];
+        for (int i = 0; i < graphCount; i++) {
+            System.out.println("Generating graph " + (i + 1) + "...");
+            Graph g = new Graph(maxVertices, maxWeight, 1);
+            verticeArr[i] = g.V;
+            edgeArr[i] = g.E;
+            long timeTotal1 = 0;
+            long timeTotal2 = 0;
+            for (int j = 0; j < averageTimes; j++) {
+                DijkstraAlgo dijkstraTest = new DijkstraAlgo(g.V);
+                long startTime = System.nanoTime();
+                dijkstraTest.dijkstraArrayStart(g.V, 0, g.adjMatrix);
+                long endTime = System.nanoTime();
+                timeTotal1 += endTime - startTime;
+
+                startTime = System.nanoTime();
+                dijkstraTest.dijkstraPQStart(g.V, 0, g.adjList);
+                endTime = System.nanoTime();
+                timeTotal2 += endTime - startTime;
+            }
+            timeArr1[i] = timeTotal1 / averageTimes;
+            timeArr2[i] = timeTotal2 / averageTimes;
+
+        }
+        MakeCSV.CSVprinter(verticeArr, "verticeArr(Sparse).csv");
+        MakeCSV.CSVprinter(edgeArr, "verticeArr(Sparse).csv");
+        MakeCSV.CSVprinter(timeArr1, "verticeArr(Sparse).csv");
+        MakeCSV.CSVprinter(timeArr2, "verticeArr(Sparse).csv");
+    }
+
+}
